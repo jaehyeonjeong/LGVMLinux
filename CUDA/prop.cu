d3c796fe346c14e703ba9ee386bf9ab24c5cc610
jaehyeonjeong <110385530+jaehyeonjeong@users.noreply.github.com>
2023-01-02 16:08:20 +0900

    tt
    tttt

--- CUDA/prop.cu
+++ CUDA/prop.cu
@@ -1,41 +1,47 @@
 #include <cuda.h>
-#include <cuda_run1me_api.h>
+#include <cuda_runtime_api.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
- cudaDeviceProp prop;
- int count;
- cudaGetDeviceCount(&count);
- for (int i=0; i< count; i++) {
- cudaGetDeviceProper1es(&prop, i);
- prinG(" --- General Informa1on for device %d ---\n", i);
- prinG("Name: %s\n", prop.name);
- prinG("Compute capability: %d.%d\n", prop.major, prop.minor);
- prinG("Clock rate: %d\n", prop.clockRate);
- prinG("Device copy overlap: ");
- prinG(prop.deviceOverlap?"Enabled\n":"Disabled\n");
- prinG("Kernel execu1on 1meout : " );
- prinG(prop.kernelExecTimeoutEnabled?"Enabled\n":"Disabled\n");
- prinG("\n" );
- prinG(" --- Memory Informa1on for device %d ---\n", i);
- prinG("Total global mem: %ld\n", prop.totalGlobalMem);
- prinG("Total constant Mem: %ld\n", prop.totalConstMem);
- prinG("Max mem pitch: %ld\n", prop.memPitch);
- prinG("Texture Alignment: %ld\n", prop.textureAlignment);
- prinG("\n");
- prinG(" --- MP Informa1on for device %d ---\n", i);
- prinG("Mul1processor count: %d\n", prop.mul1ProcessorCount);
- prinG("Shared mem per mp: %ld\n", prop.sharedMemPerBlock);
- prinG("Registers per mp: %d\n", prop.regsPerBlock);
- prinG("Threads in warp: %d\n", prop.warpSize);
- prinG("Max threads per block: %d\n", prop.maxThreadsPerBlock);
- prinG("Max thread dimensions: (%d, %d, %d)\n", prop.maxThreadsDim[0], 
- prop.maxThreadsDim[1], prop.maxThreadsDim[2]
-);
- prinG("Max grid dimensions: (%d, %d, %d)\n", prop.maxGridSize[0], 
- prop.maxGridSize[1], prop.maxGridSize[2]);
- prinG("\n");
- }
- return 0;
+   cudaDeviceProp prop;
+
+   int count;  cudaGetDeviceCount(&count);
+
+   for (int i = 0; i < count; i++) {
+      cudaGetDeviceProperties(&prop, i);
+      printf("   --- General Information for device %d ---\n", i);
+      printf("Name: %s\n", prop.name);
+      printf("Compute capability: %d.%d\n", prop.major, prop.minor);
+      printf("Clock rate: %d\n", prop.clockRate);
+
+      printf("Device copy overlap: ");
+      printf(prop.deviceOverlap ? "Enabled\n" : "Disabled\n");
+      printf("Kernel execution timeout : ");
+      printf(prop.kernelExecTimeoutEnabled ? "Enabled\n" : "Disabled\n");
+      printf("\n");
+
+      printf("   --- Memory Information for device %d ---\n", i);
+      printf("Total global mem: %ld\n", prop.totalGlobalMem);
+      printf("Total constant Mem: %ld\n", prop.totalConstMem);
+      printf("Max mem pitch: %ld\n", prop.memPitch);
+      printf("Texture Alignment: %ld\n", prop.textureAlignment);
+      printf("\n");
+
+      printf("   --- MP Information for device %d ---\n", i);
+      printf("Multiprocessor count: %d\n", prop.multiProcessorCount);
+      printf("Shared mem per mp: %ld\n", prop.sharedMemPerBlock);
+      printf("Registers per mp: %d\n", prop.regsPerBlock);
+      printf("Threads in warp: %d\n", prop.warpSize);
+      printf("Max threads per block: %d\n", prop.maxThreadsPerBlock);
+      printf("Max thread dimensions: (%d, %d, %d)\n", prop.maxThreadsDim[0],
+         prop.maxThreadsDim[1], prop.maxThreadsDim[2]
+      );
+      printf("Max grid dimensions: (%d, %d, %d)\n", prop.maxGridSize[0],
+         prop.maxGridSize[1], prop.maxGridSize[2]);
+      printf("\n");
+   }
+
+   return 0;
 }
